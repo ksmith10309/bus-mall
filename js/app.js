a6887e55d185109cdc7193deeda7ea7558bdf526
Katherine Smith <katherine.lo.smith@gmail.com>
2018-07-23 16:02:51 -0700

    Display list of products after 25 selections have been made

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ function ImageObject(img) {
   this.name = img.split('/')[1].split('.')[0];
   this.path = img;
   this.totalClicks = 0;
+  this.timesDisplayed = 0;
 }
 
 for (var i=0; i<images.length; i++) {
@@ -16,7 +17,7 @@ for (var i=0; i<images.length; i++) {
 
 var newNumbers = [];
 
-function generateNum() {
+function generateRandomNum() {
   var num = Math.floor((Math.random() * 20));
   return num;
 }
@@ -25,56 +26,104 @@ function generateNumArray() {
   var oldNumbers = newNumbers;
   newNumbers = [];
 
-  var numOne = generateNum();
+  var numOne = generateRandomNum();
   while(numOne === oldNumbers[0] || numOne === oldNumbers[1] || numOne === oldNumbers[2]) {
-    numOne = generateNum();
+    numOne = generateRandomNum();
   }
   newNumbers.push(numOne);
 
-  var numTwo = generateNum();
+  var numTwo = generateRandomNum();
   while(numTwo === numOne || numTwo === oldNumbers[0] || numTwo === oldNumbers[1] || numTwo === oldNumbers[2]) {
-    numTwo = generateNum();
+    numTwo = generateRandomNum();
   }
   newNumbers.push(numTwo);
 
-  var numThree = generateNum();
+  var numThree = generateRandomNum();
   while(numThree === numOne || numThree === numTwo || numThree === oldNumbers[0] || numThree === oldNumbers[1] || numThree === oldNumbers[2]) {
-    numThree = generateNum();
+    numThree = generateRandomNum();
   }
   newNumbers.push(numThree);
 }
 
+var clickCounter = 0;
+
 function generateImages() {
+  var mainEl = document.getElementById('main-container');
+  mainEl.textContent = '';
+
+  imageObjects[newNumbers[0]].timesDisplayed++;
   var imageEl1 = document.createElement('img');
   imageEl1.setAttribute('src', imageObjects[newNumbers[0]].path);
   imageEl1.addEventListener('click', function() {
     imageObjects[newNumbers[0]].totalClicks++;
     generateNumArray();
     generateImages();
+    clickCounter++;
+
+    if (clickCounter === 25) {
+      displayList();
+    }
   });
 
+  imageObjects[newNumbers[1]].timesDisplayed++;
   var imageEl2 = document.createElement('img');
   imageEl2.setAttribute('src', imageObjects[newNumbers[1]].path);
   imageEl2.addEventListener('click', function() {
     imageObjects[newNumbers[1]].totalClicks++;
     generateNumArray();
     generateImages();
+    clickCounter++;
+    if (clickCounter === 25) {
+      displayList();
+    }
   });
 
+  imageObjects[newNumbers[2]].timesDisplayed++;
   var imageEl3 = document.createElement('img');
   imageEl3.setAttribute('src', imageObjects[newNumbers[2]].path);
   imageEl3.addEventListener('click', function() {
     imageObjects[newNumbers[2]].totalClicks++;
     generateNumArray();
     generateImages();
+    clickCounter++;
+    if (clickCounter === 25) {
+      displayList();
+    }
   });
 
-  var mainEl = document.getElementById('main-section');
-  mainEl.textContent = '';
-  mainEl.appendChild(imageEl1);
-  mainEl.appendChild(imageEl2);
-  mainEl.appendChild(imageEl3);
+  var divEl1 = document.createElement('div');
+  divEl1.setAttribute('class', 'img-container');
+  divEl1.appendChild(imageEl1);
+
+  var divEl2 = document.createElement('div');
+  divEl2.setAttribute('class', 'img-container');
+  divEl2.appendChild(imageEl2);
+
+  var divEl3 = document.createElement('div');
+  divEl3.setAttribute('class', 'img-container');
+  divEl3.appendChild(imageEl3);
+
+  mainEl.appendChild(divEl1);
+  mainEl.appendChild(divEl2);
+  mainEl.appendChild(divEl3);
 }
 
 generateNumArray();
 generateImages();
+
+function displayList() {
+  var headingEl = document.getElementById('heading');
+  headingEl.textContent = 'These are your selections:';
+
+  var mainEl = document.getElementById('main-container');
+  mainEl.textContent = '';
+
+  var ulEl = document.createElement('ul');
+  for (var i = 0; i < imageObjects.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = imageObjects[i].totalClicks + ' votes for the ' + imageObjects[i].name;
+    ulEl.appendChild(liEl);
+  }
+
+  mainEl.appendChild(ulEl);
+}
